758884bfe79f5fdaa86cc79d26e7c32b409ce2de
spenclark <spenclark@hotmail.com>
2025-12-15 15:09:18 -0800

    Update favicon to match nav logo

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -463,7 +463,7 @@ export function Layout({ children }: { children: ReactNode }) {
         <Meta />
         <Links />
         <script type="module" src="/src/__create/dev-error-overlay.js"></script>
-        <link rel="icon" href="/src/__create/favicon.png" />
+        <link rel="icon" href="/favicon.svg" />
         {LoadFontsSSR ? <LoadFontsSSR /> : null}
       </head>
       <body>
